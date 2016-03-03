061596375c3c7a2b26a9b97cd934f25837884d27
Rachel Ehrlich <rae84@cornell.edu>
2016-03-03 13:30:06 -0500

    find kth element

--- lists.ml
+++ lists.ml
@@ -10,3 +10,8 @@ let rec last_two l =
     | [] | [ _ ] -> None
     | [ a ; b ] -> Some (a, b)
     | _ :: tl -> last_two tl
+
+let rec at k l =
+    match l with
+    | [] -> None
+    | hd :: tl -> if k = 0 then Some hd else at (k-1) tl
