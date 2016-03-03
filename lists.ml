38683e5531e29a1d5e0b762cf68b4e376db17173
Rachel Ehrlich <rae84@cornell.edu>
2016-03-03 13:45:21 -0500

    continue working through list problems

--- lists.ml
+++ lists.ml
@@ -15,3 +15,20 @@ let rec at k l =
     match l with
     | [] -> None
     | hd :: tl -> if k = 0 then Some hd else at (k-1) tl
+
+let rec length l =
+    match l with
+    | [] -> 0
+    | _ :: tl -> 1 + length tl
+
+let rev l =
+    let rec aux l1 l2 = 
+        match l1 with 
+        | [] -> l2
+        | hd :: tl -> aux tl (hd::l2)
+    in
+    aux l []
+
+
+let is_palindrome l =
+    l = rev l
